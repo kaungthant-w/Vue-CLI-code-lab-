c322ab0573a0a4fce21551fd2a21e9491c071919
kaungthant-w <kaungthant9513@gmail.com>
2023-03-07 11:25:59 +0630

    06_-_Making_Todos_list

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <h1>Hi There.</h1>
+  <div class="">
+    <h1 class="m-4 text-danger">Hello VueJs</h1>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
